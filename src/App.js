0ebc70a2a70e0738d4efabca1b268553a4df4b85
Chase <chaseburkdoll@gmail.com>
2023-06-02 10:55:40 -0500

    Added Keyboard component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Header';
 import Word from './Word';
+import Keyboard from 'react-simple-keyboard'
+import "react-simple-keyboard/build/css/index.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <main className='word-container'>
+      <main className='app--words'>
         <Word />
         <Word />
         <Word />
@@ -14,6 +16,7 @@ function App() {
         <Word />
         <Word />
       </main>
+      <Keyboard />
       
       {/*Keyboard*/}
     </div>
